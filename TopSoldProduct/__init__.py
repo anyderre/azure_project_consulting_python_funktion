df72079ae86c7fb0be992d49573744b20947dff8
anyderre <ridoreda1992@gmail.com>
2023-05-10 17:34:01 +0200

    Adding the top viewed product and top sold product

---
+++ TopSoldProduct/__init__.py
@@ -0,0 +1,33 @@
+import logging
+import pandas as pd
+from datetime import datetime
+from sqlalchemy.engine import URL
+from sqlalchemy import create_engine
+import sqlalchemy as sa
+import json
+import os
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Getting the number of sales per product.')
+
+    connection_string = os.environ['AZURE_FUNCTION_CONNECTION_STRING']
+    connection_url = URL.create(
+        "mssql+pyodbc", query={"odbc_connect": connection_string})
+    engine = create_engine(connection_url)
+
+    with engine.begin() as conn:
+        query = "SELECT top 5 product_id, sum(product_price * product_quantity) as total_sales FROM [dbo].[bestellung] group by product_id order by total_sales desc"
+
+        data = pd.read_sql_query(sa.text(query), conn)
+
+        # product_sales = data.set_index('product_id')
+
+        print(type(data))
+        parsed = json.loads(data.to_json(orient='records'))
+        return func.HttpResponse(
+            json.dumps(parsed),
+            status_code=200
+        )
